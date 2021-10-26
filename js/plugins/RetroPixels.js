e2cb3b3b8e6554653a2e0041e47b96215766a0b1
Darlos9D <Darlos9D@gmail.com>
2021-10-26 01:34:17 -0400

    getting various things in place for RetroPixels

--- js/plugins/RetroPixels.js
+++ js/plugins/RetroPixels.js
@@ -36,11 +36,99 @@
  * @default 3
  * @min 1
  * @decimals 0
+ *
+ * @param useTextImages
+ * @text Use Text Images
+ * @desc Use image files instead of fonts, for text. The best way to get pixel-accurate text.
+ * @type boolean
+ * @default false
+ *
+ * @param textImages
+ * @text Text Images
+ * @desc An array of text image files and their specifications.
+ * @type struct<textImageInfo>[]
+ * @parent useTextImages
+ *
+ * @param iconW
+ * @text Icon width
+ * @desc The width of icons, in retro pixels.
+ * @type number
+ * @default 8
+ * @min 1
+ * @decimals 0
+ *
+ * @param iconH
+ * @text Icon Height
+ * @desc The height of icons, in retro pixels.
+ * @type number
+ * @default 8
+ * @min 1
+ * @decimals 0
+ */
+ 
+/*~struct~textImageInfo:
+ *
+ * @param file
+ * @text File
+ * @desc The text image file.
+ * @type file
+ *
+ * @param characterW
+ * @text Character width
+ * @desc The width of an individual text character, in retro pixels.
+ * @type number
+ * @default 8
+ * @min 1
+ * @decimals 0
+ *
+ * @param characterH
+ * @text Character Height
+ * @desc The height of an individual text character, in retro pixels.
+ * @type number
+ * @default 8
+ * @min 1
+ * @decimals 0
  */
  
 (() => {
+	// plugin parameters
+	const rpParams = PluginManager.parameters('RetroPixels');
+	parseRPParameters();
+	
+	// helper functions
+	function parseRPParameters() {
+		rpParams.retroPixelSize = parseRPInt(rpParams.retroPixelSize, 3, 1);
+		rpParams.iconW = parseRPInt(rpParams.iconW, 8, 1);
+		rpParams.iconH = parseRPInt(rpParams.iconH, 8, 1);
+		rpParams.useTextImages = rpParams.useTextImages === 'true';
+		rpParams.textImages = JSON.parse(rpParams.textImages);
+		for(const textImageInfoStringIndex in rpParams.textImages) {
+			const textImageInfo = JSON.parse(rpParams.textImages[textImageInfoStringIndex]);
+			textImageInfo.characterW = parseRPInt(textImageInfo.characterW, 8, 1);
+			textImageInfo.characterH = parseRPInt(textImageInfo.characterH, 8, 1);
+			rpParams.textImages[textImageInfoStringIndex] = textImageInfo;
+		}
+	}
+	
+	function parseRPInt(string, defaultValue, min, max) {
+		let parsedValue = parseInt(string);
+		if(parsedValue === NaN) { return defaultValue; }
+		if(min !== undefined) { parsedValue = Math.max(min, parsedValue); }
+		if(max !== undefined) { parsedValue = Math.min(max, parsedValue); }
+		return parsedValue;
+	}
+	
 	// Bitmap
-	Bitmap.prototype.drawText = function(text, x, y, maxWidth, lineHeight, align, textImageSource) {
+	Bitmap.prototype.drawText = function(text, x, y, maxWidth, lineHeight, align) {
+		const blah = rpParams;
+		if(false) {
+			this.drawTextFromImage(text, x, y, maxWidth, lineHeight, align);
+		} else {
+			this.drawTextFromFont(text, x, y, maxWidth, lineHeight, align);
+		}
+	};
+	
+	Bitmap.prototype.drawTextFromFont = function(text, x, y, maxWidth, lineHeight, align) {
 		// [Note] Different browser makes different rendering with
 		//   textBaseline == 'top'. So we use 'alphabetic' here.
 		const context = this.context;
@@ -54,23 +142,31 @@
 		if (align === "right") {
 			tx += maxWidth;
 		}
-		if(textImageSource) {
-			this._drawTextFromImage(text, tx, ty, maxWidth, textImageSource);
-		} else {
-			context.save();
-			context.font = this._makeFontNameText();
-			context.textAlign = align;
-			context.textBaseline = "alphabetic";
-			context.globalAlpha = 1;
-			this._drawTextOutline(text, tx, ty, maxWidth, textImageFile);
-			context.globalAlpha = alpha;
-			this._drawTextBody(text, tx, ty, maxWidth);
-			context.restore();
-		}
+		context.save();
+		context.font = this._makeFontNameText();
+		context.textAlign = align;
+		context.textBaseline = "alphabetic";
+		context.globalAlpha = 1;
+		this._drawTextOutline(text, tx, ty, maxWidth);
+		context.globalAlpha = alpha;
+		this._drawTextBody(text, tx, ty, maxWidth);
+		context.restore();
 		this._baseTexture.update();
 	};
 	
-	Bitmap.prototype._drawTextFromImage = function(text, tx, ty, maxWidth, textImageSource) {
-		
+	Bitmap.prototype.drawTextFromImage = function(text, x, y, maxWidth, lineHeight, align) {
+		const context = this.context;
+		maxWidth = maxWidth || 0xffffffff;
+		let tx = x;
+		let ty = Math.round(y + lineHeight / 2 + this.fontSize * 0.35);
+		if (align === "center") {
+			tx += maxWidth / 2;
+		}
+		if (align === "right") {
+			tx += maxWidth;
+		}
+		for(const c in text) {
+			
+		}
 	};
 })();

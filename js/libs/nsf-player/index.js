c6e07f563d21d5cd737255728c91550abe6fd4d7
Darlos9D <Darlos9D@gmail.com>
2023-02-22 02:33:50 -0500

    attempting better audio handling
    gotta figure out how to get audio playing immediately

--- js/libs/nsf-player/index.js
+++ js/libs/nsf-player/index.js
@@ -1,17 +1,25 @@
 const createNsfPlayer = (audioContext) => {
-  // Messages are disabled. Feel free to handle them however you like.
-  const message = () => null;
+  const message = () => {};
+  
+  const timeCheck = (label) => {
+	const curTime = Date.now();
+	if(lastTime === undefined) {
+		console.log(label + ": first call");
+	} else {
+		console.log(label + ": " + (curTime - lastTime) + "ms since previous call");
+	}
+	
+	lastTime = curTime;
+  };
 
   const play = (fileName, trackNo) => {
-	if(fileName === curFileName) {
-	  playMusicData(curPayload, trackNo);
-	  updateSongInfo(curFileName, trackNo);
+	for(let i = 0; i < nsfDatas.length; i++) {
+		if(nsfDatas[i].fileName === fileName) {
+			playMusicData(nsfDatas[i], trackNo);
+			//updateSongInfo(fileName, trackNo);
+			return;
+		}
 	}
-	  
-    if (node) {
-      node.disconnect();
-      node = null;
-    }
 
     const xhr = new XMLHttpRequest();
     xhr.open('GET', fileName, true);
@@ -24,36 +32,47 @@ const createNsfPlayer = (audioContext) => {
         message('not found');
         return;
       }
-      const payload = new Uint8Array(this.response);
-      playMusicData(payload, trackNo);
-      updateSongInfo(fileName, trackNo);
-	  curFileName = fileName;
-	  curPayload = payload;
+	  const nsfData = {};
+	  nsfData.fileName = fileName;
+      nsfData.payload = new Uint8Array(this.response);
+	  nsfDatas[nsfDatas.length] = nsfData;
+	  
+      playMusicData(nsfData, trackNo);
+      //updateSongInfo(fileName, trackNo);
     };
     xhr.send();
   };
 
   const stop = () => {
-    if (!node || emu === undefined) {
+    if (nsfDatas.length === 0) {
       return;
     }
 
-    node.disconnect();
+	for(let i = 0; i < nsfDatas.length; i++) {
+		//if(nsfDatas[i].emu === undefined) {
+		//	continue;
+		//}
+		//Module.ccall('gme_set_tempo', 'number', ['number', 'number'], [nsfDatas[i].emu, 0.0]);
+		//Module.ccall('gme_mute_voices', 'number', ['number', 'number'], [nsfDatas[i].emu, -1]);
+		
+		if(/*nsfDatas[i].emu === undefined || nsfDatas[i].outputBuffer === undefined ||*/ nsfDatas[i].node === undefined) {
+			continue;
+		}
+		
+		nsfDatas[i].node.disconnect();
+		//nsfDatas[i].outputBuffer = undefined;
 	
-    try {
-	  Module.ccall('gme_delete', 'number', ['number'], [emu])
-	  emu = undefined;
-	} catch(exception) {
-      console.log('Failed to stop track: ' + exception);
-    }
+		//try {
+		//  Module.ccall('gme_delete', 'number', ['number'], [nsfDatas[i].emu]);
+		//  nsfDatas[i].emu = undefined;
+		//} catch(exception) {
+		//  console.log('Failed to stop track: ' + exception);
+		//}
+	}
   };
 
-  let curFileName;
-  let curPayload;
-  let ref;
-  let emu;
-  let node;
-  let ctx = audioContext;
+  let lastTime;
+  const nsfDatas = [];
 
   const parseMetadata = ref => {
     let offset = 0;
@@ -101,7 +120,9 @@ const createNsfPlayer = (audioContext) => {
     message('playing', filename, metadata);
   };
 
-  const playMusicData = (payload, subtune) => {
+  const playMusicData = (nsfData, subtune) => {
+	stop();
+	  
     if (!window.AudioContext) {
       if (window.webkitAudioContext) {
         window.AudioContext = window.webkitAudioContext;
@@ -113,32 +134,32 @@ const createNsfPlayer = (audioContext) => {
     }
 
     try {
-      ctx = ctx || new AudioContext();
+      nsfData.ctx = nsfData.ctx || new AudioContext();
     } catch(err) {
       console.error(`Unable to create AudioContext. Error: ${err}`);
       return;
     }
 
-    ref = ref || Module.allocate(1, 'i32', Module.ALLOC_STATIC);
+    nsfData.ref = nsfData.ref || Module.allocate(1, 'i32', Module.ALLOC_STATIC);
 
-    const samplerate = ctx.sampleRate;
+    const samplerate = nsfData.ctx.sampleRate;
 
-    if (Module.ccall('gme_open_data', 'number', ['array', 'number', 'number', 'number'], [payload, payload.length, ref, samplerate]) != 0) {
+    if (Module.ccall('gme_open_data', 'number', ['array', 'number', 'number', 'number'], [nsfData.payload, nsfData.payload.length, nsfData.ref, samplerate]) != 0) {
       console.error('gme_open_data failed.');
       return;
     }
 
-    emu = Module.getValue(ref, 'i32');
+    nsfData.emu = nsfData.emu || Module.getValue(nsfData.ref, 'i32');
 
-    const subtune_count = Module.ccall('gme_track_count', 'number', ['number'], [emu]);
+    //const subtune_count = Module.ccall('gme_track_count', 'number', ['number'], [nsfData.emu]);
 
-    Module.ccall('gme_ignore_silence', 'number', ['number'], [emu, 1]);
+    Module.ccall('gme_ignore_silence', 'number', ['number'], [nsfData.emu, 1]);
 
-    const voice_count = Module.ccall('gme_voice_count', 'number', ['number'], [emu]);
-    message('Channel count: ', voice_count);
-    message('Track count: ', subtune_count);
+    //const voice_count = Module.ccall('gme_voice_count', 'number', ['number'], [nsfData.emu]);
+    //message('Channel count: ', voice_count);
+    //message('Track count: ', subtune_count);
 
-    if (Module.ccall('gme_start_track', 'number', ['number', 'number'], [emu, subtune]) != 0) {
+    if (Module.ccall('gme_start_track', 'number', ['number', 'number'], [nsfData.emu, subtune]) != 0) {
       console.error('Failed to load track.');
     }
 
@@ -146,37 +167,48 @@ const createNsfPlayer = (audioContext) => {
     const inputs = 2;
     const outputs = 2;
 
-    if (!node && ctx.createJavaScriptNode) {
-      node = ctx.createJavaScriptNode(bufferSize, inputs, outputs);
+    if (!nsfData.node && nsfData.ctx.createJavaScriptNode) {
+      nsfData.node = nsfData.ctx.createJavaScriptNode(bufferSize, inputs, outputs);
     }
-    if (!node && ctx.createScriptProcessor) {
-      node = ctx.createScriptProcessor(bufferSize, inputs, outputs);
+    if (!nsfData.node && nsfData.ctx.createScriptProcessor) {
+      nsfData.node = nsfData.ctx.createScriptProcessor(bufferSize, inputs, outputs);
     }
 
     const buffer = Module.allocate(bufferSize * 2, 'i32', Module.ALLOC_STATIC);
 
     const INT16_MAX = Math.pow(2, 32) - 1;
 
-    node.onaudioprocess = (e) => {
-      if (Module.ccall('gme_track_ended', 'number', ['number'], [emu]) == 1) {
-        node.disconnect();
+    nsfData.node.onaudioprocess = (e) => {
+	  nsfData.outputBuffer = e.outputBuffer;
+      writeToOutputBuffer(e.outputBuffer);
+    };
+	
+	const writeToOutputBuffer = (outputBuffer) => {
+	  if (Module.ccall('gme_track_ended', 'number', ['number'], [nsfData.emu]) == 1) {
+        nsfData.node.disconnect();
+		nsfData.outputBuffer = undefined;
+		nsfData.emu = undefined;
         message('End of stream.');
         return;
       }
 
-      const channels = [e.outputBuffer.getChannelData(0), e.outputBuffer.getChannelData(1)];
+      const channels = [outputBuffer.getChannelData(0), outputBuffer.getChannelData(1)];
 
-      const err = Module.ccall('gme_play', 'number', ['number', 'number', 'number'], [emu, bufferSize * 2, buffer]);
+      const err = Module.ccall('gme_play', 'number', ['number', 'number', 'number'], [nsfData.emu, bufferSize * 2, buffer]);
       for (var i = 0; i < bufferSize; i++) {
-        for (var n = 0; n < e.outputBuffer.numberOfChannels; n++) {
-          channels[n][i] = Module.getValue(buffer + i * e.outputBuffer.numberOfChannels * 2 + n * 4, 'i32') / INT16_MAX;
+        for (var n = 0; n < outputBuffer.numberOfChannels; n++) {
+          channels[n][i] = Module.getValue(buffer + i * outputBuffer.numberOfChannels * 2 + n * 4, 'i32') / INT16_MAX;
         }
       }
-    }
+	};
+	
+	//Module.ccall('gme_set_tempo', 'number', ['number', 'number'], [nsfData.emu, 1.0]);
+	//Module.ccall('gme_mute_voices', 'number', ['number', 'number'], [nsfData.emu, 0]);
 
-    node.connect(ctx.destination);
-
-    window.savedReferences = [ctx, node];
+    nsfData.node.connect(nsfData.ctx.destination);
+	if(nsfData.outputBuffer !== undefined) {
+		writeToOutputBuffer(nsfData.outputBuffer);
+	}
   };
 
   return { play, stop };
